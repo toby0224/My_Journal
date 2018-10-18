1ba947b195f5b16a4ce7806f0c91c8bbb7c6a3ff
Toby Huynh <tobymhuynh0224@gmail.com>
2018-10-18 14:53:46 -0400

    test commit

--- app/src/main/java/com/example/toby/my_journal/MainActivity.java
+++ app/src/main/java/com/example/toby/my_journal/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // does nothing
+
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
